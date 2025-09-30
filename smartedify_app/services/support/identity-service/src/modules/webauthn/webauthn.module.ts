a03ddaf03ab5ef913e8b82240b49aa991f844d61
egomez11 <91491126+egomez11@users.noreply.github.com>
2025-09-30 20:26:19 +0000

    fix(identity-service): integra todos los cambios funcionales y de pruebas validados por e2e y specs

--- smartedify_app/services/support/identity-service/src/modules/webauthn/webauthn.module.ts
+++ smartedify_app/services/support/identity-service/src/modules/webauthn/webauthn.module.ts
@@ -1,15 +1,44 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { WebAuthnCredential } from './entities/webauthn-credential.entity';
 import { WebauthnService } from './webauthn.service';
+import {
+  generateRegistrationOptions,
+  verifyRegistrationResponse,
+  generateAuthenticationOptions,
+  verifyAuthenticationResponse,
+} from '@simplewebauthn/server';
 import { WebauthnController } from './webauthn.controller';
 import { RpService } from './rp.service';
 import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 import { ChallengeStoreService } from './store/challenge-store.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([WebAuthnCredential]), UsersModule],
-  providers: [WebauthnService, RpService, ChallengeStoreService],
+  providers: [
+    {
+      provide: WebauthnService,
+      useFactory: (
+        rpService: RpService,
+        usersService: UsersService,
+        webAuthnCredentialRepository: Repository<WebAuthnCredential>,
+      ) =>
+        new WebauthnService(
+          rpService,
+          usersService,
+          webAuthnCredentialRepository,
+          generateRegistrationOptions,
+          verifyRegistrationResponse,
+          generateAuthenticationOptions,
+          verifyAuthenticationResponse,
+        ),
+      inject: [RpService, UsersService, getRepositoryToken(WebAuthnCredential)],
+    },
+    RpService,
+    ChallengeStoreService,
+  ],
   controllers: [WebauthnController],
 })
 export class WebauthnModule {}
